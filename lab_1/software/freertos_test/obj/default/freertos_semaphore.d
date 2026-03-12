6f084494f1f36939abb47e6a8404f0cbf418e569
Samuel Lee <ylee903@UoA.auckland.ac.nz>
2026-03-12 15:58:11 +1300

    blah

--- lab_1/software/freertos_test/obj/default/freertos_semaphore.d
+++ lab_1/software/freertos_test/obj/default/freertos_semaphore.d
@@ -1,8 +1,9 @@
 obj/default/freertos_semaphore.o: freertos_semaphore.c \
+ freertos/FreeRTOS.h freertos/FreeRTOSConfig.h \
  ../freertos_test_bsp/system.h ../freertos_test_bsp/linker.h \
- freertos/FreeRTOS.h freertos/FreeRTOSConfig.h freertos/projdefs.h \
- freertos/portable.h freertos/deprecated_definitions.h \
- freertos/portmacro.h ../freertos_test_bsp//HAL/inc/sys/alt_irq.h \
+ freertos/projdefs.h freertos/portable.h \
+ freertos/deprecated_definitions.h freertos/portmacro.h \
+ ../freertos_test_bsp//HAL/inc/sys/alt_irq.h \
  ../freertos_test_bsp//HAL/inc/nios2.h \
  ../freertos_test_bsp//HAL/inc/alt_types.h \
  ../freertos_test_bsp//HAL/inc/priv/alt_legacy_irq.h \
@@ -12,13 +13,13 @@ obj/default/freertos_semaphore.o: freertos_semaphore.c \
  ../freertos_test_bsp//HAL/inc/io.h \
  ../freertos_test_bsp//HAL/inc/alt_types.h
 
-../freertos_test_bsp/system.h:
-
-../freertos_test_bsp/linker.h:
-
 freertos/FreeRTOS.h:
 
 freertos/FreeRTOSConfig.h:
+
+../freertos_test_bsp/system.h:
+
+../freertos_test_bsp/linker.h:
 
 freertos/projdefs.h:
 
