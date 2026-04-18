943394cb4fa2e48771997e22504ae607ad4adf51
Samuel Lee <ylee903@UoA.auckland.ac.nz>
2026-04-18 17:02:49 +1200

    only measurng the time between first isr and first load shed

---
+++ Assignment_1/software/AssignmentOne/obj/default/FreeRTOS/event_groups.d
@@ -0,0 +1,45 @@
+obj/default/FreeRTOS/event_groups.o: FreeRTOS/event_groups.c \
+ FreeRTOS/FreeRTOS.h FreeRTOS/FreeRTOSConfig.h \
+ ../AssignmentOne_bsp/system.h ../AssignmentOne_bsp/linker.h \
+ FreeRTOS/projdefs.h FreeRTOS/portable.h \
+ FreeRTOS/deprecated_definitions.h FreeRTOS/portmacro.h \
+ ../AssignmentOne_bsp//HAL/inc/sys/alt_irq.h \
+ ../AssignmentOne_bsp//HAL/inc/nios2.h \
+ ../AssignmentOne_bsp//HAL/inc/alt_types.h \
+ ../AssignmentOne_bsp//HAL/inc/priv/alt_legacy_irq.h \
+ FreeRTOS/mpu_wrappers.h FreeRTOS/task.h FreeRTOS/list.h \
+ FreeRTOS/timers.h FreeRTOS/event_groups.h
+
+FreeRTOS/FreeRTOS.h:
+
+FreeRTOS/FreeRTOSConfig.h:
+
+../AssignmentOne_bsp/system.h:
+
+../AssignmentOne_bsp/linker.h:
+
+FreeRTOS/projdefs.h:
+
+FreeRTOS/portable.h:
+
+FreeRTOS/deprecated_definitions.h:
+
+FreeRTOS/portmacro.h:
+
+../AssignmentOne_bsp//HAL/inc/sys/alt_irq.h:
+
+../AssignmentOne_bsp//HAL/inc/nios2.h:
+
+../AssignmentOne_bsp//HAL/inc/alt_types.h:
+
+../AssignmentOne_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+FreeRTOS/mpu_wrappers.h:
+
+FreeRTOS/task.h:
+
+FreeRTOS/list.h:
+
+FreeRTOS/timers.h:
+
+FreeRTOS/event_groups.h:
---
+++ Assignment_1/software/AssignmentOne/obj/default/FreeRTOS/port.d
@@ -0,0 +1,51 @@
+obj/default/FreeRTOS/port.o: FreeRTOS/port.c \
+ ../AssignmentOne_bsp//HAL/inc/sys/alt_irq.h \
+ ../AssignmentOne_bsp//HAL/inc/nios2.h \
+ ../AssignmentOne_bsp//HAL/inc/alt_types.h ../AssignmentOne_bsp/system.h \
+ ../AssignmentOne_bsp/linker.h \
+ ../AssignmentOne_bsp//HAL/inc/priv/alt_legacy_irq.h \
+ ../AssignmentOne_bsp//drivers/inc/altera_avalon_timer_regs.h \
+ ../AssignmentOne_bsp//HAL/inc/io.h \
+ ../AssignmentOne_bsp//HAL/inc/alt_types.h \
+ ../AssignmentOne_bsp//HAL/inc/priv/alt_irq_table.h FreeRTOS/FreeRTOS.h \
+ FreeRTOS/FreeRTOSConfig.h FreeRTOS/projdefs.h FreeRTOS/portable.h \
+ FreeRTOS/deprecated_definitions.h FreeRTOS/portmacro.h \
+ FreeRTOS/mpu_wrappers.h FreeRTOS/task.h FreeRTOS/list.h
+
+../AssignmentOne_bsp//HAL/inc/sys/alt_irq.h:
+
+../AssignmentOne_bsp//HAL/inc/nios2.h:
+
+../AssignmentOne_bsp//HAL/inc/alt_types.h:
+
+../AssignmentOne_bsp/system.h:
+
+../AssignmentOne_bsp/linker.h:
+
+../AssignmentOne_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+../AssignmentOne_bsp//drivers/inc/altera_avalon_timer_regs.h:
+
+../AssignmentOne_bsp//HAL/inc/io.h:
+
+../AssignmentOne_bsp//HAL/inc/alt_types.h:
+
+../AssignmentOne_bsp//HAL/inc/priv/alt_irq_table.h:
+
+FreeRTOS/FreeRTOS.h:
+
+FreeRTOS/FreeRTOSConfig.h:
+
+FreeRTOS/projdefs.h:
+
+FreeRTOS/portable.h:
+
+FreeRTOS/deprecated_definitions.h:
+
+FreeRTOS/portmacro.h:
+
+FreeRTOS/mpu_wrappers.h:
+
+FreeRTOS/task.h:
+
+FreeRTOS/list.h:
